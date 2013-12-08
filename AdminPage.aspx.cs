cd9db14e77ecace58926fdfb3dbf587503422104
qiangli3409 <qiang.li@mint-corp.com>
2013-12-07 20:10:35 -0600

    vto first commit cs

---
+++ AdminPage.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class AdminHome : System.Web.UI.Page
+{
+    private test9.mobilewebserviceSoapClient ws;
+     
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ws = new test9.mobilewebserviceSoapClient();
+        if (!IsPostBack)
+        {
+            BindData();
+
+           
+        }
+    }
+
+    private void BindData()
+    {
+
+
+        DataSet ds = ws.VTOGetAllAccount();
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+
+    }
+      
+
+    protected void DeleteAccount(object sender, EventArgs e)
+    {
+        /*LinkButton lnkRemove = (LinkButton)sender;
+        SqlConnection con = new SqlConnection(strConnString);
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "delete from  customers where " +
+        "CustomerID=@CustomerID;" +
+         "select CustomerID,ContactName,CompanyName from customers";
+        cmd.Parameters.Add("@CustomerID", SqlDbType.VarChar).Value = lnkRemove.CommandArgument;
+        GridView1.DataSource = GetData(cmd);
+        GridView1.DataBind(); */
+        LinkButton lnkRemove = (LinkButton)sender;
+        DataSet ds = ws.VTODeleteAccountByID(Convert.ToInt32(lnkRemove.CommandArgument));
+        if (ds == null)
+        {
+            lblmsg.Text = "Delete Users under this account first";
+            return;
+        }
+
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+
+        lblmsg.Text = "Delete accountID " + lnkRemove.CommandArgument ;
+
+    }
+
+    protected void AddNewAccount(object sender, EventArgs e)
+    {
+      
+
+        string bname = ((TextBox)GridView1.FooterRow.FindControl("txtaccount_bname")).Text;
+        if (string.IsNullOrEmpty(bname)) { lblmsg.Text = "empty account_bname"; return; }
+
+        string fname = ((TextBox)GridView1.FooterRow.FindControl("txtaccount_fname")).Text;
+        if (string.IsNullOrEmpty(bname)) { lblmsg.Text = "empty account_fname"; return; }
+
+        string lname = ((TextBox)GridView1.FooterRow.FindControl("txtaccount_lname")).Text;
+        if (string.IsNullOrEmpty(bname)) { lblmsg.Text = "empty account_lname"; return; }
+
+        string phone = ((TextBox)GridView1.FooterRow.FindControl("txtaccount_phone")).Text;
+        if (string.IsNullOrEmpty(bname)) { lblmsg.Text = "empty account_phone"; return; }
+
+        string email = ((TextBox)GridView1.FooterRow.FindControl("txtaccount_email")).Text;
+        if (string.IsNullOrEmpty(bname)) { lblmsg.Text = "empty account_email"; return; }
+
+        string icon = ((TextBox)GridView1.FooterRow.FindControl("txtaccount_icon")).Text;
+        if (string.IsNullOrEmpty(bname)) { lblmsg.Text = "empty account_icon"; return; }
+
+        string url = ((TextBox)GridView1.FooterRow.FindControl("txtaccount_url")).Text;
+        if (string.IsNullOrEmpty(bname)) { lblmsg.Text = "empty account_url"; return; }
+
+        // do not know why I can not read the datastring
+        // string startdate = ((TextBox)GridView1.FooterRow.FindControl("lblaccount_startdate")).Text;
+        string enabled = ((TextBox)GridView1.FooterRow.FindControl("txtaccount_enabled")).Text;
+        if (string.IsNullOrEmpty(bname)) { lblmsg.Text = "empty account_enalbed"; return; }
+
+        
+        bool result = enabled.Equals("true", StringComparison.OrdinalIgnoreCase);
+
+
+        DataSet ds = ws.VTOAddNewAccount(bname, fname, lname, phone, email, icon, url, result);
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+
+
+        lblmsg.Text = "add new account for " + bname;
+      }
+
+    protected void ManageUsers(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void OnPaging(object sender, GridViewPageEventArgs e)
+    {
+        BindData();
+        GridView1.PageIndex = e.NewPageIndex;
+        GridView1.DataBind();
+        lblmsg.Text = "";
+    }
+
+    protected void EditAccount(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        BindData();
+        lblmsg.Text = "";
+    }
+    protected void CancelEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        BindData();
+        lblmsg.Text = "";
+    }
+    protected void UpdateAccount(object sender, GridViewUpdateEventArgs e)
+    {
+
+       
+       string account_id = ((Label)GridView1.Rows[e.RowIndex].FindControl("lblaccount_id")).Text;
+       string bname = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtaccount_bname")).Text;
+       string fname = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtaccount_fname")).Text;
+       string lname = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtaccount_lname")).Text;
+       string phone = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtaccount_phone")).Text;
+       string email = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtaccount_email")).Text;
+       string icon = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtaccount_icon")).Text;
+       string url = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtaccount_url")).Text;
+        // do not know why I can not read the datastring
+      // string startdate = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("lblaccount_startdate")).Text;
+       string enabled = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtaccount_enabled")).Text;
+
+        int id = Convert.ToInt32(account_id);
+
+        bool result = enabled.Equals("true", StringComparison.OrdinalIgnoreCase);
+        
+   
+       
+        GridView1.EditIndex = -1;
+        DataSet ds = ws.VTOUpdateAccount(id, bname, fname, lname, phone, email, icon, url, result);
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+
+        lblmsg.Text = "update accountID " + account_id + " successfully";
+    }
+
+
+    
+}
---
+++ Home.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+   
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+ 
+    }
+    protected void ValidateLogin(string username, string password)
+    {
+
+        test9.mobilewebserviceSoapClient ws = new test9.mobilewebserviceSoapClient();
+
+        DataSet dsUser = ws.VTOUserLogin(username, password);
+
+        if (dsUser != null && dsUser.Tables[0].Rows.Count > 0)
+        {
+            if (Convert.ToBoolean(dsUser.Tables[0].Rows[0]["user_isenabled"]))
+            {
+                Session["User"] = dsUser;
+                DataRow drUser = dsUser.Tables[0].Rows[0];
+               txtUserName.Text = string.Empty;
+               txtPassword.Text = string.Empty;
+                if (Convert.ToString(dsUser.Tables[0].Rows[0]["user_type"]) == "ADMIN")
+                        Response.Redirect("AdminPage.aspx");
+                    else
+                        Response.Redirect("UserPage.aspx");
+                //RedirectByAccess();
+            }
+            else
+            {
+                lblError.Text = "Incorrect password.";
+                lblError.Enabled = true;
+                lblError.Visible = true;
+            }
+        }
+        else
+        {
+            
+            lblError.Text = "Invalid username or password.";
+            lblError.Enabled = true;
+            lblError.Visible = true;
+            //Response.Redirect("home.aspx");
+        }
+        
+    }
+
+    protected void btlogin_Click(object sender, EventArgs e)
+    {
+        ValidateLogin(txtUserName.Text.Trim(), txtPassword.Text.Trim());
+    }
+
+
+}
+/*
+using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+public partial class how_to_login_with_sql_server_c : System.Web.UI.Page
+{
+    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        string username = Login1.UserName;
+        string pwd = Login1.Password;
+
+         test9.mobilewebserviceSoapClient ws = new test9.mobilewebserviceSoapClient();
+
+          DataSet ds = ws.VTOGetAllAccount();
+       
+
+      string strConn;
+        strConn = WebConfigurationManager.ConnectionStrings["ConnectionASPX"].ConnectionString;
+        SqlConnection Conn = new SqlConnection(strConn);
+        Conn.Open();
+
+        string sqlUserName;
+        sqlUserName = "SELECT UserName,Password FROM UserName ";
+        sqlUserName += " WHERE (UserName ='" + username + "')";
+        sqlUserName += " AND (Password ='" + pwd + "')";
+        SqlCommand com = new SqlCommand(sqlUserName, Conn);
+      
+        string CurrentName;
+        CurrentName = (string)com.ExecuteScalar();
+
+        if (CurrentName != null)
+        {
+            Session["UserAuthentication"] = username;
+            Session.Timeout = 1;
+            Response.Redirect("how-to-StartPage.aspx");
+        }
+        else
+        {
+            Session["UserAuthentication"] = "";
+        }
+    }
+}*/
